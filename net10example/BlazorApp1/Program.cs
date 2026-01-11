18f107f1098eb5a3f0e110d9b143982557f73c1e
karo <karolin.kostial@gmail.com>
2026-01-11 11:03:59 +0100

    net 10 example

---
+++ net10example/BlazorApp1/Program.cs
@@ -0,0 +1,37 @@
+using BlazorApp1.Components;
+using BlazorApp1.Components.Pages;
+using FluentBlazorRouter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddFluentRouting<Home>(rootBuilder => rootBuilder
+    .WithPage<Counter>("counter/{Id:int}")
+    .WithGroup("group/example", exampleGroupBuilder =>
+    {
+        exampleGroupBuilder.WithPage<Weather>("fetchdata");
+    }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
